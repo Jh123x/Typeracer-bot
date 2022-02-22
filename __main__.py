1d9e9a96911d75291620621fce91d334b71a23e9
Wen Junhua <wenjh1998@gmail.com>
2022-02-22 16:55:38 -0500

    complete bot

--- __main__.py
+++ __main__.py
@@ -4,7 +4,8 @@ import pytesseract
 
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
+tesseract_config = r"""-c tessedit_char_whitelist="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ,:.-'?" """
+tesseract_language = "eng"
 
 def screenshot() -> Image:
     """Screenshot"""
@@ -14,7 +15,7 @@ def screenshot() -> Image:
 
 def enter_text(words: str):
     """Enter the string"""
-    pyautogui.typewrite(words)
+    pyautogui.typewrite(words, interval=0.05)
 
 
 def alt_tab():
@@ -24,10 +25,10 @@ def alt_tab():
 
 def crop_image(image: Image) -> Image:
     """Resize the image"""
-    return image.crop((1950, 1400, 3750, 1700))
+    return image.crop((1950, 1250, 3750, 1700))
 
 def ocr(image: Image) -> str:
-    return pytesseract.image_to_string(image)
+    return pytesseract.image_to_string(image.convert("1"), lang=tesseract_language, config=tesseract_config)
 
 
 def main():
@@ -35,9 +36,13 @@ def main():
     img = crop_image(img)
 
     typed_string = ocr(img)
-    print(typed_string)
+    # print(typed_string)
+    temp = typed_string.split("\n\n")
+    typed_str = " ".join(temp[0].split("\n"))
+    print(typed_str)
     alt_tab()
-    enter_text(''.join(typed_string))
+    enter_text(typed_str)
+    img.show()
 
 
 if __name__ == '__main__':
